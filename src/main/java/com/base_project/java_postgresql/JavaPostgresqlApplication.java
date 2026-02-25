79279f5450bdfa8693943b494d486898ca1279bd
Fajar <fajar.anwarimaulana99@gmail.com>
2026-02-25 16:46:11 +0700

    Feat: Initial Commit

---
+++ src/main/java/com/base_project/java_postgresql/JavaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.base_project.java_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base_project/java_postgresql/JavaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base_project.java_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
